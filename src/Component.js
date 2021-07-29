4b38c78e6207172839a9bdedb1451e89795b600f
wushufen <wusfun@foxmail.com>
2021-07-29 23:06:22 +0800

    ...

---
+++ src/Component.js
@@ -0,0 +1,18 @@
+class Component{
+  constructor() {
+    this.$forPath = ''
+    this.$wrapper = null
+    this.$node = null
+    this.$template = ''
+
+    this.$render = function(props = {}) {
+      
+    }
+  }
+  $(id){}
+  mount(){}
+}
+
+class IndexComponent{
+
+}
